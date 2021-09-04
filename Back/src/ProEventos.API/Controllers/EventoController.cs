838b41074b3874bab6703e559ceb526d3f7fcc3f
Danilo Daltro <danilodaltro@hotmail.com>
2021-09-04 00:27:45 -0300

    "Segundo commit no ProEventos"

--- Back/src/ProEventos.API/Controllers/EventoController.cs
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using ProEventos.API.Data;
 using ProEventos.API.Models;
 
 namespace ProEventos.API.Controllers
@@ -12,42 +13,25 @@ namespace ProEventos.API.Controllers
     [Route("api/[controller]")]
     public class EventoController : ControllerBase
     {
-        public IEnumerable<Evento> _evento = new Evento[] {
-            new Evento(){
-                EventoId = 1,
-                Tema = ".NET 5 e Angular 11",
-                Local = "Salvador",
-                QtdPessoas = 300,
-                Lote = "1",
-                DataEvento = DateTime.Now.AddDays(2).ToString("dd/mm/yyyy"),
-                ImagemURL = "foto1.png"
-            },
-            new Evento(){
-                EventoId = 2,
-                Tema = "SQL Server",
-                Local = "Feira de Santana",
-                QtdPessoas = 500,
-                Lote = "3",
-                DataEvento = DateTime.Now.AddDays(5).ToString("dd/mm/yyyy"),
-                ImagemURL = "foto3.png"
+        private readonly DataContext dataContext;
 
-            }
-        };
-
-        public EventoController()
+        public EventoController(DataContext dataContext)
         {
+            this.dataContext = dataContext;
         }
 
         [HttpGet]
         public IEnumerable<Evento> Get()
         {
-            return _evento;
+            return dataContext.Eventos;
         }
 
         [HttpGet("{id}")]
-        public IEnumerable<Evento> Get(int id){
-            return _evento.Where(x => x.EventoId.Equals(id));
+        public Evento GetById(int id)
+        {
+            return dataContext.Eventos.FirstOrDefault(evento => evento.EventoId == id);
         }
+
 
         [HttpPost]
         public string Post()
