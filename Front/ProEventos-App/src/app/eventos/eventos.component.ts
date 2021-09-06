eebd18a596217c68c9ef14ef0fd7850e0cfae2ec
Danilo Daltro <danilodaltro@hotmail.com>
2021-09-06 00:20:25 -0300

    Subindo Sessão 5 - Angular

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -8,7 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: any;
+  public eventos: any = [];
+  public eventosFiltrados: any = [];
+  public widthImg: number = 150;
+  public marginImg: number = 2;
+  public exibirImg: boolean = true;
+  private _filtroLista: string = "";
 
   constructor(private http: HttpClient) { }
 
@@ -16,9 +21,34 @@ export class EventosComponent implements OnInit {
     this.getEventos();
   }
 
+  alterarExibicaoImagem(): void{
+    this.exibirImg = !this.exibirImg;
+  }
+
+  public get filtroLista(): string{
+    return this._filtroLista;
+  }
+
+  public set filtroLista(value: string){
+    this._filtroLista = value;
+    this.eventosFiltrados = this._filtroLista ? this.filtrarEventos(this._filtroLista) : this.eventos;
+  }
+
+  public filtrarEventos(filtrarPor: string): any{
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento: any) => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+      || evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    );
+  }
+
   public getEventos(): void {
     this.http.get('https://localhost:5001/api/eventos').subscribe(
-      response => this.eventos = response,
+      response =>
+      {
+        this.eventos = response;
+        this.eventosFiltrados = this.eventos;
+      },
       error => console.log(error)
     )
   }
